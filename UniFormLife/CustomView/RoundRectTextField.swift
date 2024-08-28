15e90653740aa44d24537e1fdc757f2b9bbc01e5
yoonwooiOS <yoonwoo4429@kakao.com>
2024-08-28 23:06:35 +0900

    🐛 fix : RoundRectTextField Toolbar 메서드 추가

--- UniFormLife/CustomView/RoundRectTextField.swift
+++ UniFormLife/CustomView/RoundRectTextField.swift
@@ -19,12 +19,34 @@ final class RoundRectTextField: UITextField {
         layer.borderWidth = 1.0
         layer.cornerRadius = 5.0
         self.inputView = inputView
+        addDoneToolbar()
     }
-    
+    private func addDoneToolbar() {
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        let cancelButton = UIBarButtonItem(title: "취소", style: .plain, target: self, action: #selector(cancelButtonTapped))
+        cancelButton.tintColor = Color.red
+        let doneButton = UIBarButtonItem(title: "확인", style: .done, target: self, action: #selector(doneButtonTapped))
+        doneButton.tintColor = Color.black
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        
+        // 툴바에 버튼 추가
+        toolbar.setItems([cancelButton, flexSpace, doneButton], animated: true)
+        self.inputAccessoryView = toolbar
+        
+    }
+    @objc private func cancelButtonTapped() {
+        self.text = ""
+        self.resignFirstResponder()
+    }
+    @objc private func doneButtonTapped() {
+        self.resignFirstResponder()
+    }
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     
 }
