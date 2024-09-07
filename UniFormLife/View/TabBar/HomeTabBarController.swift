201f48dd0aa665fe4ced8c01997d7ec97b466023
yoonwooiOS <yoonwoo4429@kakao.com>
2024-09-07 22:39:43 +0900

    ✨ feat : PostView 중고거래, 피드 추가 분기 처리

--- UniFormLife/View/TabBar/HomeTabBarController.swift
+++ UniFormLife/View/TabBar/HomeTabBarController.swift
@@ -7,15 +7,15 @@
 
 import UIKit
 
-final class HomeTabBarController: UITabBarController, UITabBarControllerDelegate{
+final class HomeTabBarController: UITabBarController, UITabBarControllerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let apearance = UITabBarAppearance()
-        apearance.configureWithOpaqueBackground()
+        let appearance = UITabBarAppearance()
+        appearance.configureWithOpaqueBackground()
         tabBar.tintColor = Color.black
-        tabBar.standardAppearance = apearance
-        tabBar.scrollEdgeAppearance = apearance
+        tabBar.standardAppearance = appearance
+        tabBar.scrollEdgeAppearance = appearance
         tabBar.unselectedItemTintColor = Color.gray
         
         let uniformListVC = UniformListViewController()
@@ -24,8 +24,8 @@ final class HomeTabBarController: UITabBarController, UITabBarControllerDelegate
         
         let feedVC = UniformStyleFeedViewController()
         let feed = UINavigationController(rootViewController: feedVC)
-        feedVC.tabBarItem = UITabBarItem(title: "", image: Image.TabBar.feed, tag: 1)
-//        AddPostViewController()
+        feed.tabBarItem = UITabBarItem(title: "", image: Image.TabBar.feed, tag: 1)
+
         let addPostVC = AddPostViewController()
         let addPost = UINavigationController(rootViewController: addPostVC)
         addPost.tabBarItem = UITabBarItem(title: "", image: Image.TabBar.addPost, tag: 2)
@@ -33,17 +33,50 @@ final class HomeTabBarController: UITabBarController, UITabBarControllerDelegate
         let profileVC = ProfileViewController()
         let profile = UINavigationController(rootViewController: profileVC)
         profile.tabBarItem = UITabBarItem(title: "", image: Image.TabBar.profile, tag: 3)
+        
         delegate = self
-        setViewControllers([uniformList, addPost, feed, profile], animated: true)
+        setViewControllers([uniformList, feed, addPost, profile], animated: true)
     }
-//    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
-//        if viewController.tabBarItem.tag == 2 {
-//            let vc = AddPostViewController()
-//            let nav = UINavigationController(rootViewController: vc)
-//            nav.modalPresentationStyle = .fullScreen
-//            present(nav, animated: true)
-//            return false
-//        }
-//        return true
-//    }
+
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        if viewController.tabBarItem.tag == 2 {
+            showPostOptions()
+            return false
+        }
+        return true
+    }
+
+    private func showPostOptions() {
+        let alertController = UIAlertController(title: "게시물 작성", message: "어떤 유형의 게시물을 작성하시겠습니까?", preferredStyle: .actionSheet)
+
+        let usedProductAction = UIAlertAction(title: "중고제품 등록하기", style: .default) { [weak self] _ in
+            self?.presentUsedProductVC()
+        }
+
+        let feedAction = UIAlertAction(title: "피드 등록하기", style: .default) { [weak self] _ in
+            self?.presentFeedVC()
+        }
+
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+
+        alertController.addAction(usedProductAction)
+        alertController.addAction(feedAction)
+        alertController.addAction(cancelAction)
+
+        present(alertController, animated: true, completion: nil)
+    }
+
+    private func presentUsedProductVC() {
+        let usedProductVC = AddPostViewController() // 이 클래스는 별도로 만들어야 합니다
+        let nav = UINavigationController(rootViewController: usedProductVC)
+        nav.modalPresentationStyle = .fullScreen
+        present(nav, animated: true)
+    }
+
+    private func presentFeedVC() {
+        let feedVC = AddFeedViewController() // 이 클래스는 별도로 만들어야 합니다
+        let nav = UINavigationController(rootViewController: feedVC)
+        nav.modalPresentationStyle = .fullScreen
+        present(nav, animated: true)
+    }
 }
